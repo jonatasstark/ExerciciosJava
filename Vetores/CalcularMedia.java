a3f6eb7c7aef78b401aafd1c47909124a508289b
jonatasstark <jonatasg856@gmail.com>
2023-10-18 17:07:09 -0300

    Exercicios Gabriel Lucas e Jonatas

--- Vetores/CalcularMedia.java
+++ Vetores/CalcularMedia.java
@@ -1,25 +1,35 @@
 package Vetores;
 import java.util.Scanner;
+
 public class CalcularMedia {
+
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
-        double media = 0, soma = 0;
 
         System.out.println("Quantas notas serão digitadas: ");
         int numeros = ler.nextInt();
 
-        double notas;
+        double[] notas = new double[numeros]; // Array para armazenar as notas
+
         System.out.println("Digite as notas: ");
-        for (int i = 0; i < numeros; i ++){
-            notas = ler.nextDouble();
-            soma += notas;
-
-            media = soma / numeros;
-
+        for (int i = 0; i < numeros; i++) {
+            notas[i] = ler.nextDouble();
         }
 
         ler.close();
 
+        double media = calcularMedia(notas); // Chama a função para calcular a média
+
         System.out.println("A média das notas é de: " + media);
+    }
+
+    public static double calcularMedia(double[] notas) {
+        double soma = 0;
+
+        for (int i = 0; i < notas.length; i++) {
+            soma += notas[i];
+        }
+
+        return soma / notas.length;
     }
 }
